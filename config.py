d37077f564a5414bafd89c9e833ef95018e6f777
Enes Tezcan <tezcan_enes@hotmail.com>
2022-02-24 20:17:29 +0300

    last

--- config.py
+++ config.py
@@ -18,9 +18,11 @@ app.config['PROPAGATE_EXCEPTIONS'] = True
 # app.config["SESSION_PERMANENT"] = False
 app.config["PERMANENT_SESSION_LIFETIME "] = timedelta(minutes= 30)
 app.config["SESSION_TYPE"] = "filesystem"
+
+
 app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = os.environ.get(address)
-app.config['MAIL_PASSWORD'] = os.environ.get(password)
+app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
+app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
 app.config['MAIL_DEFAULT_SENDER'] = address
