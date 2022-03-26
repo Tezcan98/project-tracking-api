e538fae8b53a24f73badaa2aa7334822e244d898
Enes Tezcan <tezcan_enes@hotmail.com>
2022-03-26 18:04:35 +0000

    latest

--- Dockerfile
+++ Dockerfile
@@ -3,4 +3,4 @@ COPY requirements.txt /
 RUN pip install -r /requirements.txt
 COPY . /app
 WORKDIR /app
-CMD [ "python", "app.py" ]
+CMD [ "flask", "run" ]
--- app.py
+++ app.py
@@ -2,15 +2,15 @@ from flask import Flask, render_template, request, jsonify, session, redirect, u
 from flask_session import Session
 from db_procs import *
 
+app = Flask(__name__)
 from config import *
 
+Session(app)
 
 @app.route("/")
 def index():
 	return render_template("dashboard.html")
 
 if __name__ == "__main__":
-	app = Flask(__name__)
-	Session(app)
 	app.run(static_url_path='', static_folder='static', template_folder='templates', host='0.0.0.0')
 	
