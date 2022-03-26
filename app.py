89edc5ff8602214733c98bc1b8d168e7e5806b85
Enes Tezcan <tezcan_enes@hotmail.com>
2022-03-26 17:50:51 +0000

    latest

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request, jsonify, session, redirect, u
 from flask_session import Session
 from db_procs import *
 
-app = Flask(__name__, static_url_path='', static_folder='static', template_folder='templates' )
+app = Flask(__name__)
 from config import *
 
 Session(app)
@@ -12,5 +12,5 @@ def index():
 	return render_template("dashboard.html")
 
 if __name__ == "__main__":
-	app.run()
+	app.run(static_url_path='', static_folder='static', template_folder='templates' )
 	
